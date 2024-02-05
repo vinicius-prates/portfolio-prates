60d91eb4a9069b6e93b7b73a92e1f426e93c9e0f
vinicius-prates <viniprates2211@gmail.com>
2024-02-05 09:11:19 -0300

    new: restart portifolio

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Navbar } from "./components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Vinicius Prates",
+  description: "Portifólio Vinicius Prates",
+  icons: "/letra-v.png",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <header>
+          <Navbar />
+        </header>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
